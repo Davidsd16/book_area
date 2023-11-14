6d03c91e286bbd707408af8d7fc8fca5b1d4ba90
david <davidsanchezdiaz72@gmail.com>
2023-11-14 23:09:24 +0100

    estilos

--- resources/views/livewire/books.blade.php
+++ resources/views/livewire/books.blade.php
@@ -1,23 +1,25 @@
 <div>
     {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
-
-    <table>
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+    <table class="table-fixed w-full">
         <thead>
-            <tr>
-                <th>Title</th>
-                <th>Author</th>
-                <th>Description</th>
-                <th>Price</th>
+            <tr class="bg-indigo-600 text-white">
+                <th class="px-4 py-2">Title</th>
+                <th class="px-4 py-2">Author</th>
+                <th class="px-4 py-2">Description</th>
+                <th class="px-4 py-2">Price</th>
             </tr>
         </thead>
 
         <tbody>
             @foreach($books as $book)
             <tr>
-                <td>{{$book->title}}</td>
-                <td>{{$book->author}}</td>
-                <td>{{$book->description}}</td>
-                <td>{{$book->price}}</td>
+                <td class="border px-4 py-2">{{$book->title}}</td>
+                <td class="border px-4 py-2">{{$book->author}}</td>
+                <td class="border px-4 py-2">{{$book->description}}</td>
+                <td class="border px-4 py-2">{{$book->price}}</td>
             </tr>
             @endforeach
         </tbody>
