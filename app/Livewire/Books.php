fd2c7bfcb39cf0d4cc0b70e718425f9937f3cf5e
david <davidsanchezdiaz72@gmail.com>
2023-11-15 01:09:28 +0100

    Aplicamos estilos a nuestra plantilla principal //  Botón y procedimiento para Crear

--- app/Livewire/Books.php
+++ app/Livewire/Books.php
@@ -8,13 +8,33 @@ use App\Models\Book;
 class Books extends Component
 {
 
-    public $books;
+    public $books, $title, $author, $description, $price;
+    public $modal = false;
 
     public function render()
     {
         $this->books = Book::all();
-       // var_dump($this->books);
-       // die();
         return view('livewire.books');
+    }
+
+    public function crear()
+    {
+        $this->limpiarCampos();
+        $this->abrirModal();
+    }
+
+    public function abrirModal() {
+        $this->modal = true;
+    }
+
+    public function cerrarModal() {
+        $this->modal = false;
+    }
+
+    public function limpiarCampos(){
+        $this->title = '';
+        $this->author = '';
+        $this->description = '';
+        $this->price = '';
     }
 }
--- resources/views/livewire/books.blade.php
+++ resources/views/livewire/books.blade.php
@@ -1,27 +1,43 @@
-<div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    <table class="table-fixed w-full">
-        <thead>
-            <tr class="bg-indigo-600 text-white">
-                <th class="px-4 py-2">Title</th>
-                <th class="px-4 py-2">Author</th>
-                <th class="px-4 py-2">Description</th>
-                <th class="px-4 py-2">Price</th>
-            </tr>
-        </thead>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+@vite('resources/css/app.css')
 
-        <tbody>
-            @foreach($books as $book)
-            <tr>
-                <td class="border px-4 py-2">{{$book->title}}</td>
-                <td class="border px-4 py-2">{{$book->author}}</td>
-                <td class="border px-4 py-2">{{$book->description}}</td>
-                <td class="border px-4 py-2">{{$book->price}}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+<x-slot name="header">
+    <h1 class="text-gray-900">Area de libros esenciales</h1>
+</x-slot>
+
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px6 lg:px8">
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4">
+
+        <button wire:click="crear()"
+        class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 my-3">
+        Nuevo
+        </button>
+        @if(modal)
+            @include('liveweri.crear')
+        @endif    
+            <table class="table-fixed w-full">
+                <thead>
+                    <tr class="bg-indigo-600 text-white">
+                        <th class="px-4 py-2">Title</th>
+                        <th class="px-4 py-2">Author</th>
+                        <th class="px-4 py-2">Description</th>
+                        <th class="px-4 py-2">Price $</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    @foreach($books as $book)
+                    <tr>
+                        <td class="border px-4 py-2">{{$book->title}}</td>
+                        <td class="border px-4 py-2">{{$book->author}}</td>
+                        <td class="border px-4 py-2">{{$book->description}}</td>
+                        <td class="border px-4 py-2">{{$book->price}}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
